41127268879ef7b124d696cef326f3ef6f56a0aa
LeBellier <Brusuf@orange.fr>
2017-02-15 20:29:09 +0100

    Clean up with eclipse

--- CodeArduino/ESPTable/ArtNet.h
+++ CodeArduino/ESPTable/ArtNet.h
@@ -3,8 +3,7 @@
 
 #include <ArtnetWifi.h>
 #include <Aspect.h>
-//ArtnetWifi artnet;// if you are on Arduino IDE
-extern ArtnetWifi artnet; // if you are on Eclipse
+ArtnetWifi artnet;
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence,
 		uint8_t* data) {
