54ed0af282a3e7a586c4636096ed691cd4db76b6
LeBellier <Brusuf@orange.fr>
2017-02-15 19:33:41 +0100

    CLean up the code
    And Create an excel to dont forget task

--- CodeArduino/ESPTable/ESPTable.ino
+++ CodeArduino/ESPTable/ESPTable.ino
@@ -1,16 +1,16 @@
 /*
-  Programme pour table à led
-  Bruno Bellier
-*/
+ Programme pour table à led
+ Bruno Bellier
+ */
 
 //#define FTP_DEBUG
 //#define DEBUG_ESP
-//#define DEBUG_INIT
+#define DEBUG_INIT
 #include <Aspect.h>
 
 #include "PersonnalData.h"
 #include <MatrixStrip.h>
-// MatrixStrip strip(pixelPin, nbRows, nbColumns); // il faut les personnalData + MatrixStrip
+//MatrixStrip strip(pixelPin, nbRows, nbColumns); // il faut les personnalData + MatrixStrip
 #include "Animations.h"// besoin de IPAdress
 #include <ServersWeb.h> // personnalData
 #include <WifiManager.h> // personnalData
@@ -18,35 +18,35 @@
 #include "PagesWeb.h"
 
 void setup(void) {
-  Serial.begin(BAUDRATE);
+	Serial.begin(BAUDRATE);
 
-  pinMode( led, OUTPUT);
-  digitalWrite( led, 0);
-  strip.rgbBlink();
+	pinMode( led, OUTPUT);
+	digitalWrite( led, 0);
+	strip.rgbBlink();
 
-  initWIFI();
-  delayIfDebug(15);
+	initWIFI();
+	delayIfDebug(15);
 
-  artnet.setArtDmxCallback(onDmxFrame);
-  artnet.begin();
-  initDnsHttpFtpOtaServers();
-  delayIfDebug(20);
-  httpServer.on("/pixel", HTTP_GET, pixelRequest);
+	artnet.setArtDmxCallback(onDmxFrame);
+	artnet.begin();
+	initDnsHttpFtpOtaServers();
+	delayIfDebug(20);
+	httpServer.on("/pixel", HTTP_GET, pixelRequest);
 
-  if (WiFi.getMode() != WIFI_STA) {
-    showIP(WiFi.softAPIP());
-  } else {
-    showIP(WiFi.localIP());
-  }
+	if (WiFi.getMode() != WIFI_STA) {
+		showIP(WiFi.softAPIP());
+	} else {
+		showIP(WiFi.localIP());
+	}
 
-  digitalWrite( led, 1);
+	digitalWrite( led, 1);
 }
 void loop() {
-  updateServers();
-  artnet.read();
-  if (animate == true) {
-    startShow(showType);
-    animate = false;
-  }
+	updateServers();
+	artnet.read();
+	if (animate == true) {
+		startShow(showType);
+		animate = false;
+	}
 }
 
